3760ceae8749c920738cbbdbb4eae135f5633b09
Fen747 <davidpanart@gmail.com>
2015-11-29 15:36:36 +0100

    modules

---
+++ client/_modules/cm_game.js
@@ -0,0 +1,19 @@
+let instance = null;
+
+let create = ( ) => {
+
+};
+
+let update = ( ) => {
+
+};
+
+let prelaod = ( ) => {
+
+};
+
+Modules.client.Game           = {};
+Modules.client.Game.update    = update;
+Modules.client.Game.create    = create;
+Modules.client.Game.prelaod   = prelaod;
+Modules.client.Game.instance  = instance;
